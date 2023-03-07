9bad1ac5ef5c112968716b964039205538135bd1
Marco Cabral <64281795+Mcabral18@users.noreply.github.com>
2023-03-07 15:10:14 +0000

    Update home.php

--- page-templates/home.php
+++ page-templates/home.php
@@ -19,7 +19,7 @@ get_header();
     <div class="<?php echo esc_attr(get_theme_mod('understrap_container_type')); ?>" id="content">
         <div class="main text-center">
             <div>
-                <img src="https://legendary.pt/app/uploads/2018/08/brand-horizontal-300x34.png" alt="Legendary Theme">
+                <p>Custom Theme</p>
             </div>
         </div>
     </div>
